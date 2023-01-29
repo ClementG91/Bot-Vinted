195fb9e412aeab61190cb6d115d2f81f977672da
ClementG91 <123742385+ClementG91@users.noreply.github.com>
2023-01-29 19:21:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import os
 
 for i in range (1000):
-    url = 'https://www.vinted.fr/femmes/accessoires/bijoux/boucles-doreilles/2431920694-boucle-doreilles'
+    url = ''
     webbrowser.open_new_tab(url)
     time.sleep(5)
     os.system("taskkill /im brave.exe /f")
